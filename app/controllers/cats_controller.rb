2ea06c065f3971cf5e82004f02458276dacfc53a
George Groh <george.groh@gmail.com>
2014-04-23 14:49:18 -0700

    completed cat forms

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -11,4 +11,14 @@ class CatsController < ApplicationController
     @cat = Cat.new(params[:cat])
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :color, :age, :birth_date, :sex)
+  end
+
 end
